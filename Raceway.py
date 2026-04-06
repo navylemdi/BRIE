e28642a80cf143c53e55ea89f7531c72a4c42ec3
Curuon <dilemyvan@yahoo.fr>
2026-04-06 15:30:07 +0200

    Adding a race diameter different to d_fit which is the fitting (shaft or housing diameter)

--- Raceway.py
+++ Raceway.py
@@ -4,14 +4,15 @@ import Material
 class Raceway():
     """Raceway object class
     """
-    def __init__(self, Material:Material, type: str, r:float, d:float, b: float, ds : float):
+    def __init__(self, Material:Material, type: str, r:float, d:float, d_fit:float, b: float, ds : float):
         """Initialization of the Raceway object
 
         Args:
             Material (Material): Material of the raceway
             type (str): Type of the racweay (Inner or Outer)
             r (float): Raceway groove curvature radius [m]
-            d (float): Raceway inner or outer diameter [m]
+            d (float): Race diameter [m]
+            d_fit (float): Raceway inner or outer diameter [m]
             b (float): Raceway width [m]
             ds (float): Shoulder diameter [m]
         """
@@ -20,6 +21,7 @@ class Raceway():
         self.check_type()
         self.r = r
         self.d = d
+        self.d_fit = d_fit
         self.b = b
         self.ds = ds
 
